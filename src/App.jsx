bdf9d1c6be4e2f612ef07f7e87c48cd4f5e89613
DavidQRy <davidgui393@gmail.com>
2025-12-22 21:04:13 -0500

    feat: add cat facts app with image display and styling
    - Added App component to fetch and display random cat facts and images.
    - Integrated CSS for layout and styling.
    - Updated main entry point to render the App component.
    - Added ESLint configuration with Standard style guide.

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+const CAT_ENPOINT_RANDOM_CAT = 'https://catfact.ninja/fact'
+// const CAT_ENPOINT_IMAGE_URL = `https://cataas.com/cat/says/${fistWord}?size=50&color=red&json=true`
+
+const App = () => {
+  const [fact, setFact] = useState()
+
+  const [imageUrl, setImageUrl] = useState()
+  useEffect(() => {
+    fetch(CAT_ENPOINT_RANDOM_CAT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+      })
+  }, [])
+
+  useEffect(() => {
+    if (!fact) return
+
+    const firsWord = fact.split(' ').slice(0, 3).join(' ')
+    fetch(`https://cataas.com/cat/says/${firsWord}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+      })
+  }, [fact])
+  return (
+   <main>
+    <h1>App of cats</h1>
+    <section>
+    {fact && <p>{fact}</p>}
+    {imageUrl && <img src={imageUrl} alt={fact} />}
+
+    </section>
+  </main>
+  )
+}
+
+export default App
